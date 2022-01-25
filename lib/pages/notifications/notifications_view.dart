d50a1f3ee08370086c5ee57823227fb811e54101
Motasem Z. AbuNema <52855540+MotasemZiad@users.noreply.github.com>
2022-01-25 10:22:54 +0200

    some updates

--- lib/pages/notifications/notifications_view.dart
+++ lib/pages/notifications/notifications_view.dart
@@ -10,74 +10,74 @@ class NotificationsView extends GetView<NotificationsController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Colors.grey[200],
         body: SafeArea(
-      child: Column(
-        children: [
-          const AppBarWidget(
-            isBackButton: false,
-            title: 'Notifications',
-          ),
-          Expanded(
-            child: SingleChildScrollView(
-              physics: const BouncingScrollPhysics(),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.all(12),
-                    child: Text(
-                      'New',
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.black,
-                        fontWeight: FontWeight.w900,
-                      ),
-                    ),
-                  ),
-                  Container(
-                    color: Colors.grey[200],
-                    child: ListView.separated(
-                      shrinkWrap: true,
-                      itemBuilder: (context, index) => NotificationWidget(
-                        notification: controller.notifications[index],
-                      ),
-                      separatorBuilder: (context, index) =>
-                          const SizedBox(height: 8),
-                      itemCount: controller.notifications.length - 2,
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  Padding(
-                    padding: const EdgeInsets.all(12),
-                    child: Text(
-                      DateFormat('EEEE, dd MMMM yyyy').format(DateTime.now()),
-                      style: const TextStyle(
-                        fontSize: 18,
-                        color: Colors.black,
-                        fontWeight: FontWeight.w900,
-                      ),
-                    ),
-                  ),
-                  Container(
-                    color: Colors.grey[200],
-                    child: ListView.separated(
-                      shrinkWrap: true,
-                      itemBuilder: (context, index) => NotificationWidget(
-                        notification: controller.readNotifications[index],
-                        isRead: true,
-                      ),
-                      separatorBuilder: (context, index) =>
-                          const SizedBox(height: 8),
-                      itemCount: controller.readNotifications.length,
-                    ),
-                  ),
-                  const SizedBox(height: 12),
-                ],
+          child: Column(
+            children: [
+              const AppBarWidget(
+                isBackButton: false,
+                title: 'Notifications',
               ),
-            ),
+              Expanded(
+                child: SingleChildScrollView(
+                  physics: const BouncingScrollPhysics(),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(12),
+                        child: const Text(
+                          'New',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w900,
+                          ),
+                        ),
+                      ),
+                      ListView.separated(
+                        shrinkWrap: true,
+                        itemBuilder: (context, index) => NotificationWidget(
+                          notification: controller.notifications[index],
+                        ),
+                        separatorBuilder: (context, index) =>
+                            const SizedBox(height: 8),
+                        itemCount: controller.notifications.length - 2,
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(12),
+                        child: Text(
+                          DateFormat('EEEE, dd MMMM yyyy')
+                              .format(DateTime.now()),
+                          style: const TextStyle(
+                            fontSize: 18,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w900,
+                          ),
+                        ),
+                      ),
+                      ListView.separated(
+                        shrinkWrap: true,
+                        itemBuilder: (context, index) => NotificationWidget(
+                          notification: controller.readNotifications[index],
+                          isRead: true,
+                        ),
+                        separatorBuilder: (context, index) =>
+                            const SizedBox(height: 8),
+                        itemCount: controller.readNotifications.length,
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
-    ));
+        ));
   }
 }
